17082364a1a46d27aa7968c28fc031a88e932836
Gavin Acquroff <gavina352@gmail.com>
2021-07-20 22:49:33 +0000

    implement eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = require('./assertEqual');
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+// Tests
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
