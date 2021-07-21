9bdfb6c552daade121092d70acba1107ee9e1091
Gavin Acquroff <gavina352@gmail.com>
2021-07-21 21:44:11 +0000

    Implement findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = require("./assertEqual");
+
+const findKeyByValue = (obj, value) => {
+  for (const key in obj) {
+    if (obj[key] === value) return key;
+  }
+
+  return undefined;
+};
+
+// Tests
+if (process.argv[2] === "testFindKeyByValue") {
+  const bestTVShowsByGenre = {
+    sciFi: "The Expanse",
+    comedy: "Brooklyn Nine-Nine",
+    drama:  "The Wire",
+    wiring:  "The Wire",
+    number: 1,
+    undef: undefined,
+    array: [1, "foo"]
+  };
+
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+  assertEqual(findKeyByValue(bestTVShowsByGenre, 1), "number");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, undefined), "undef");
+  assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "wiring"); // Fail because should return first key with given value
+  assertEqual(findKeyByValue({}, "foo"), undefined);
+  assertEqual(findKeyByValue(bestTVShowsByGenre, [1, "foo"]), "array"); // Fail because does not work with array values
+}
