7cc8ac7929bcc96ed1fbd073f1fcfad69c507e30
Gavin Acquroff <gavina352@gmail.com>
2021-07-21 22:58:01 +0000

    Implement eqObjects. Add nested array capability to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,13 @@
 const assertEqual = require('./assertEqual');
 
 const eqArrays = (arr1, arr2) => {
-  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+  if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
     return false;
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i])) return eqArrays(arr1[i], arr2[i]);
+    else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -20,6 +21,9 @@ if (process.argv[2] === "testEqArrays") {
   assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
   assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
   assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", 4]), false);
+  assertEqual(eqArrays([1, [2, [3, 5]]], [1, [2, [3, 5]]]), true);
+  assertEqual(eqArrays([1, [2, [3, 5]]], [1, [2, [3, "5"]]]), false);
 }
 
 module.exports = eqArrays;
---
+++ eqObjects.js
@@ -0,0 +1,46 @@
+const assertEqual = require("./assertEqual");
+const eqArrays = require("./eqArrays");
+
+const isObj = obj => (typeof obj !== "object" || obj === null) ? false : true;
+
+const eqObjects = (obj1, obj2) => {
+  if (!isObj(obj1) || !isObj(obj2) ||
+      Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+
+  // Check primitives
+  for (let key in obj1) {
+    if (Array.isArray(obj1[key])) {
+      if (!eqArrays(obj1[key], obj2[key])) return false;
+    } else if (isObj(obj1[key])) {
+      return eqObjects(obj1[key], obj2[key]);
+    } else {
+      if (obj1[key] !== obj2[key]) return false;
+    }
+  }
+
+  return true;
+};
+
+// Tests
+if (process.argv[2] === "testEqObjects") {
+  const ab = { a: "1", b: "2" };
+  const ba = { b: "2", a: "1" };
+  assertEqual(eqObjects(ab, ba), true); // => true
+
+  const abc = { a: "1", b: "2", c: "3" };
+  assertEqual(eqObjects(ab, abc), false); // => false
+
+  const arrObj1 = {a: [1, 2], b: 3};
+  const arrObj2 = {b: 3, a: [1, 2]};
+  assertEqual(eqObjects(arrObj1, arrObj2), true);
+
+  const objObj1 = {a: {"foo": 1, "bar": "baz"}, b: 3};
+  const objObj2 = {b: 3, a: {"foo": 1, "bar": "baz"}};
+  assertEqual(eqObjects(objObj1, objObj2), true);
+
+  const nestObj1 = {a: {"foo": 1, "bar": {"bop": [1, 2]}}, b: 3};
+  const nestObj2 = {b: 3, a: {"foo": 1, "bar": {"bop": [1, 2]}}};
+  const nestObj3 = {b: 3, a: {"foo": 1, "bar": {"bop": [1, 2, [1]]}}};
+  assertEqual(eqObjects(nestObj1, nestObj2), true);
+  assertEqual(eqObjects(nestObj1, nestObj3), false);
+}
