37689a8db1572e6effce60f918e7469b18b0c3a8
Gavin Acquroff <gavina352@gmail.com>
2021-07-21 01:07:42 +0000

    Implement flatten. Handle undefined input in eqArrays.

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,10 @@
 const assertEqual = require('./assertEqual');
 
 const eqArrays = (arr1, arr2) => {
+  if (!arr1 || !arr2) {
+    return false;
+  }
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
---
+++ flatten.js
@@ -0,0 +1,23 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
+const flatten = array => {
+  let flattenedArray = [];
+  
+  array.forEach(elem => {
+    if (Array.isArray(elem)) {
+      flattenedArray = flattenedArray.concat(flatten(elem));
+    } else {
+      flattenedArray.push(elem);
+    }
+  });
+  
+  return flattenedArray;
+};
+
+// Tests
+if (process.argv[2] === "testFlatten") {
+  assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  assertArraysEqual(flatten([1, [2, [3, [4, [5, 6]]], 7]]), [1, 2, 3, 4, 5, 6, 7]); // Deeply nested arrays
+  assertArraysEqual(flatten([1, 2, [], 3]), [1, 2, 3]);
+  assertArraysEqual(flatten([1, 2, [1, [3, [], [5, "foo"]], 3]]), [1, 2, 1, 3, 5, "foo", 3]);
+}
