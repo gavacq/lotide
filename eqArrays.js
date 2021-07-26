a004df5e8434afee6fa26c6ab757ad4ec6de47f3
Gavin Acquroff <gavina352@gmail.com>
2021-07-26 21:04:55 +0000

    refactor eqArrays tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,3 @@
-const assertEqual = require('./assertEqual');
-const inspect = require("util").inspect;
-
 const eqArrays = (arr1, arr2) => {
   if (!Array.isArray(arr1) || !Array.isArray(arr2) || arr1.length !== arr2.length) {
     return false;
@@ -10,28 +7,11 @@ const eqArrays = (arr1, arr2) => {
     if (Array.isArray(arr1[i])) {
       if (!eqArrays(arr1[i], arr2[i])) return false;
     } else if (arr1[i] !== arr2[i]) {
-      console.log("Array 1:", inspect(arr1));
-      console.log("Array 2:", inspect(arr2));
-
       return false;
     }
   }
 
   return true;
 };
-
-// Tests
-if (process.argv[2] === "testEqArrays") {
-  assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-  assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-  assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3", 4]), false);
-  assertEqual(eqArrays([1, [2, [3, 5]]], [1, [2, [3, 5]]]), true);
-  assertEqual(eqArrays([1, [2, [3, 5]]], [1, [2, [3, "5"]]]), false);
-  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-  assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
-}
 
 module.exports = eqArrays;
