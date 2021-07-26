1048ca8789c30b82bc1a5dac31eb86406f3228a6
Gavin Acquroff <gavina352@gmail.com>
2021-07-26 20:58:53 +0000

    Refactor assertEqual and Head with new test dir

--- assertEqual.js
+++ assertEqual.js
@@ -8,11 +8,3 @@ const assertEqual = function(actual, expected) {
 };
 
 module.exports = assertEqual;
-
-// TEST CODE
-if (process.argv[2] === "testAssertEqual") {
-  assertEqual("Lighthouse Labs", "Bootcamp");
-  assertEqual(1, 1);
-  assertEqual("foo123", "foo123");
-  assertEqual(42, 69);
-}
--- head.js
+++ head.js
@@ -1,11 +1,5 @@
-const assertEqual = require("./assertEqual");
-
 const head = (array) => {
   return array[0];
 };
 
 module.exports = head;
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["foo"]), "foo");
-// assertEqual(head([]), undefined);
