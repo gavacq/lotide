b8a3d85d5ccc84e13d44b515fc2fc965b4f54e95
Gavin Acquroff <gavina352@gmail.com>
2021-07-26 21:01:29 +0000

    refactor tail tests

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-// const assertEqual = require("./assertEqual");
-
 const tail = (array) => {
-  return array.slice(1,array.length);
+  return array.slice(1, array.length);
 };
 
 module.exports = tail;
-
-// Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// const wordsTail = tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
-// assertEqual(wordsTail[0], words[1]);
-// assertEqual(wordsTail[1], words[2]);
-
-// const singleElem = ["single"];
-// assertEqual(tail(singleElem).length, 0);
-
-// const emptyArray = [];
-// assertEqual(tail(emptyArray).length, 0);
---
+++ test/tailTest.js
@@ -0,0 +1,15 @@
+const assertEqual = require("../assertEqual");
+const tail = require("../tail");
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+const wordsTail = tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(wordsTail[0], words[1]);
+assertEqual(wordsTail[1], words[2]);
+
+const singleElem = ["single"];
+assertEqual(tail(singleElem).length, 0);
+
+const emptyArray = [];
+assertEqual(tail(emptyArray).length, 0);
