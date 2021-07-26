28485e541182aaca7cef6d395a08126810dcb4c9
Gavin Acquroff <gavina352@gmail.com>
2021-07-26 21:11:25 +0000

    refactor middle tests

--- middle.js
+++ middle.js
@@ -1,5 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
 const middle = array => {
   if (array.length <= 2) {
     return [];
@@ -10,10 +8,4 @@ const middle = array => {
   }
 };
 
-// Tests
-if (process.argv[2] === "testMiddle") {
-  assertArraysEqual(middle([1]), []);
-  assertArraysEqual(middle([1, 2]), []);
-  assertArraysEqual(middle([1, 2, 3]), [2]);
-  assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-}
+module.exports = middle;
