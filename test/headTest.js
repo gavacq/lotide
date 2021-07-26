90c8d65072265edd9c449019849f29b2c74b6f52
Gavin Acquroff <gavina352@gmail.com>
2021-07-26 21:26:11 +0000

    refactor headTest to use Mocha + Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,17 @@
-const assertEqual = require("../assertEqual");
+const expect = require("chai").expect;
 const head = require("../head");
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["foo"]), "foo");
-assertEqual(head([]), undefined);
+describe("#head", () => {
+  it("should return the first number from an array of numbers", () =>{
+    expect(head([5, 6, 7])).to.equal(5);
+  });
+  it("should return the first string in an array of strings", () => {
+    expect(head(["Hello", "Lighthouse", "Labs"])).to.equal("Hello");
+  });
+  it("should return the only element in a single element array", () => {
+    expect(head(["foo"])).to.equal("foo");
+  });
+  it("should return undefined for an empty array", () => {
+    expect(head([])).to.equal(undefined);
+  });
+});
